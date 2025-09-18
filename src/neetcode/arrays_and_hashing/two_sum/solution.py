a2a99414cc199234e337954c0544f5f953114a55
Rohan Veit <rohan@lappop.powerhub>
2025-09-18 15:30:57 +0100

    two sum

---
+++ src/neetcode/arrays_and_hashing/two_sum/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = dict()
+        for idx, item in enumerate(nums):
+            complement = target - item
+            if complement in seen.keys():
+                return [idx, seen[complement]]
+            seen[item] = idx
+        
+        
+        
