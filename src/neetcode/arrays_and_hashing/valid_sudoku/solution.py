52c163adf258a4a9a02c2fe89e497e824ef2bc13
rohanveit <rohan.veit@gmail.com>
2025-11-03 18:32:04 +0000

    feat: added implementation of valid_sudoku

--- src/neetcode/arrays_and_hashing/valid_sudoku/solution.py
+++ src/neetcode/arrays_and_hashing/valid_sudoku/solution.py
@@ -6,22 +6,19 @@ class Solution(object):
         """
         seen = set()
         # horizontal
-        for row in board:
-            seen = set()
-            for num in row:
-                if num not in seen:
-                    seen.add(num)
-                elif num != '.':
-                    return False
+        for row_idx, row in enumerate(board):
+            for column_idx, number in enumerate(row):
+                # disregard 'empty' squares
+                if number == '.':
+                    continue
 
-        seen = set()
-        for i in range(len(board)):
-            seen = set()
-            for row in board:
-                if row[i] not in seen:
-                    seen.add(num)
-                elif num != '.':
-                    return False
+                msg1 = f"{number} seen in row {row_idx}"
+                msg2 = f"{number} seen in column {column_idx}"
+                msg3 = f"{number} seen in sub-square ({row_idx // 3},{column_idx // 3})"
+                all_msgs = [msg1, msg2, msg3]
 
-        for i in range(0, len(board), 3):
-            pass
+                if any(msg in seen for msg in all_msgs):
+                    return False
+                seen.update(all_msgs)
+
+        return True
